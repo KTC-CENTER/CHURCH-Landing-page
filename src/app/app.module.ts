19c66f261a16f30aeaa87b3fbdb84859c6685e59
NanaIvana <cindynana346@gmail.com>
2024-12-19 17:56:43 +0100

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { FeaturesComponent } from './features/features.component';
+import { Feature1Component } from './feature1/feature1.component';
+import { FunctionComponent } from './function/function.component';
+import { Feature2Component } from './feature2/feature2.component';
+import { SectionComponent } from './section/section.component';
+import { Feature3Component } from './feature3/feature3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    FeaturesComponent,
+    Feature1Component,
+    FunctionComponent,
+    Feature2Component,
+    SectionComponent,
+    Feature3Component
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feature3/feature3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature3',
+  templateUrl: './feature3.component.html',
+  styleUrl: './feature3.component.scss'
+})
+export class Feature3Component {
+
+}
